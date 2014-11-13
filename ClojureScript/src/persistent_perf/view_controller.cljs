423de3117d22154b8234580ceb39f2e130f60f11
Mike Fikes <mike@fikesfarm.com>
2014-11-13 15:36:10 -0500

    Conventional formatting of loop/recur

--- ClojureScript/src/persistent_perf/view_controller.cljs
+++ ClojureScript/src/persistent_perf/view_controller.cljs
@@ -4,11 +4,9 @@
 (defui obj-c-time-label cljs-time-label run-tests-button)
 
 (defn- run-cljs-test []
-  (loop [i 0
-         m (transient {})]
+  (loop [i 0 m (transient {})]
     (if (< i 1000000)
-      (recur (inc i)
-             (conj! m [i i]))
+      (recur (inc i) (conj! m [i i]))
       (persistent! m))))
 
 (defn- run-tests! []
